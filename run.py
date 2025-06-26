dea4598ca13356ff0c78e7402857d2dbc881b72e
Dragun-op <kumarviajyant525@gmail.com>
2025-06-27 02:09:13 +0530

    Removed init-db route after Supabase initialization

--- run.py
+++ run.py
@@ -5,11 +5,5 @@ from sqlalchemy import text
 
 app = create_app()
 
-@app.route('/init-db')
-def init_db():
-    with app.app_context():
-        db.create_all()
-    return "✅ Database initialized!"
-
 if __name__ == '__main__':
     app.run( port=5001)
